119ace2609dc1ba93b19337eb15c3002e0186996
Juan Grasso <juangrassob@gmil.com>
2021-07-08 15:42:02 -0300

    v1

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+
+const MongoStore = require('connect-mongo');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(session({
+    secret: 'El secreto',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: 'mongodb://localhost/tutorial_db',
+        collectionName: 'sessions'
+    }),
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 // 1 dia
+    }
+}));
+
+//Creo un middleware en el que agrego un componente extra a la session
+app.use('/', (req, res, next) => {
+    req.session.info = 'Aca puedo poner la informacion que quiera a travez de un middelware';
+    next();
+})
+
+
+app.get('/', (req, res, next) => {
+    res.send(req.session);
+})
+
+app.listen(3000);
